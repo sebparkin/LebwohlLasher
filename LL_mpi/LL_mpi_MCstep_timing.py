49e4b1b5406c1dd1bb19f55078a12cdb16820c57
sebparkin <sebastianparkin@gmail.com>
2025-02-24 15:44:59 +0000

    added comm.bcast to this to make sure the updated lattice was being broadcast to each core

--- LL_mpi/LL_mpi_MCstep_timing.py
+++ LL_mpi/LL_mpi_MCstep_timing.py
@@ -1,36 +1,8 @@
-"""
-Basic Python Lebwohl-Lasher code.  Based on the paper 
-P.A. Lebwohl and G. Lasher, Phys. Rev. A, 6, 426-429 (1972).
-This version in 2D.
-
-Run at the command line by typing:
-
-python LebwohlLasher.py <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>
-
-where:
-  ITERATIONS = number of Monte Carlo steps, where 1MCS is when each cell
-      has attempted a change once on average (i.e. SIZE*SIZE attempts)
-  SIZE = side length of square lattice
-  TEMPERATURE = reduced temperature in range 0.0 - 2.0.
-  PLOTFLAG = 0 for no plot, 1 for energy plot and 2 for angle plot.
-  
-The initial configuration is set at random. The boundaries
-are periodic throughout the simulation.  During the
-time-stepping, an array containing two domains is used; these
-domains alternate between old data and new data.
-
-SH 16-Oct-23
-"""
-
-import sys
 import time
-import datetime
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 from mpi4py import MPI
+import pandas as pd
 
-#=======================================================================
 def initdat(nmax):
     """
     Arguments:
@@ -45,90 +17,7 @@ def initdat(nmax):
     arr = np.random.random_sample((nmax,nmax))*2.0*np.pi
     return arr
 #=======================================================================
-def plotdat(arr,pflag,nmax):
-    """
-    Arguments:
-	  arr (float(nmax,nmax)) = array that contains lattice data;
-	  pflag (int) = parameter to control plotting;
-      nmax (int) = side length of square lattice.
-    Description:
-      Function to make a pretty plot of the data array.  Makes use of the
-      quiver plot style in matplotlib.  Use pflag to control style:
-        pflag = 0 for no plot (for scripted operation);
-        pflag = 1 for energy plot;
-        pflag = 2 for angles plot;
-        pflag = 3 for black plot.
-	  The angles plot uses a cyclic color map representing the range from
-	  0 to pi.  The energy plot is normalised to the energy range of the
-	  current frame.
-	Returns:
-      NULL
-    """
-    if pflag==0:
-        return
-    u = np.cos(arr)
-    v = np.sin(arr)
-    x = np.arange(nmax)
-    y = np.arange(nmax)
-    cols = np.zeros((nmax,nmax))
-    if pflag==1: # colour the arrows according to energy
-        mpl.rc('image', cmap='rainbow')
-        for i in range(nmax):
-            for j in range(nmax):
-                cols[i,j] = one_energy(arr,i,j,nmax)
-        norm = plt.Normalize(cols.min(), cols.max())
-    elif pflag==2: # colour the arrows according to angle
-        mpl.rc('image', cmap='hsv')
-        cols = arr%np.pi
-        norm = plt.Normalize(vmin=0, vmax=np.pi)
-    else:
-        mpl.rc('image', cmap='gist_gray')
-        cols = np.zeros_like(arr)
-        norm = plt.Normalize(vmin=0, vmax=1)
 
-    quiveropts = dict(headlength=0,pivot='middle',headwidth=1,scale=1.1*nmax)
-    fig, ax = plt.subplots()
-    q = ax.quiver(x, y, u, v, cols,norm=norm, **quiveropts)
-    ax.set_aspect('equal')
-    plt.show()
-#=======================================================================
-def savedat(arr,nsteps,Ts,runtime,ratio,energy,order,nmax):
-    """
-    Arguments:
-	  arr (float(nmax,nmax)) = array that contains lattice data;
-	  nsteps (int) = number of Monte Carlo steps (MCS) performed;
-	  Ts (float) = reduced temperature (range 0 to 2);
-	  ratio (float(nsteps)) = array of acceptance ratios per MCS;
-	  energy (float(nsteps)) = array of reduced energies per MCS;
-	  order (float(nsteps)) = array of order parameters per MCS;
-      nmax (int) = side length of square lattice to simulated.
-    Description:
-      Function to save the energy, order and acceptance ratio
-      per Monte Carlo step to text file.  Also saves run data in the
-      header.  Filenames are generated automatically based on
-      date and time at beginning of execution.
-	Returns:
-	  NULL
-    """
-    # Create filename based on current date and time.
-    current_datetime = datetime.datetime.now().strftime("%a-%d-%b-%Y-at-%I-%M-%S%p")
-    filename = "LL-Output-{:s}.txt".format(current_datetime)
-    FileOut = open(filename,"w")
-    # Write a header with run parameters
-    print("#=====================================================",file=FileOut)
-    print("# File created:        {:s}".format(current_datetime),file=FileOut)
-    print("# Size of lattice:     {:d}x{:d}".format(nmax,nmax),file=FileOut)
-    print("# Number of MC steps:  {:d}".format(nsteps),file=FileOut)
-    print("# Reduced temperature: {:5.3f}".format(Ts),file=FileOut)
-    print("# Run time (s):        {:8.6f}".format(runtime),file=FileOut)
-    print("#=====================================================",file=FileOut)
-    print("# MC step:  Ratio:     Energy:   Order:",file=FileOut)
-    print("#=====================================================",file=FileOut)
-    # Write the columns of data
-    for i in range(nsteps+1):
-        print("   {:05d}    {:6.4f} {:12.4f}  {:6.4f} ".format(i,ratio[i],energy[i],order[i]),file=FileOut)
-    FileOut.close()
-#=======================================================================
 def one_energy(arr,ix,iy,nmax):
     """
     Arguments:
@@ -271,7 +160,7 @@ def MC_step(arr,Ts,nmax, grid):
     return accept/(nmax*nmax)
 
 #=======================================================================
-def main(program, nsteps, nmax, temp, pflag):
+def main(nsteps, nmax, temp, comm):
     """
     Arguments:
 	  program (string) = the name of the program;
@@ -285,7 +174,6 @@ def main(program, nsteps, nmax, temp, pflag):
       NULL
     """
     #initialise MPI
-    comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
     
@@ -303,9 +191,7 @@ def main(program, nsteps, nmax, temp, pflag):
 
     # Create and initialise lattice
     lattice = initdat(nmax)
-    # Plot initial frame of lattice
-    if rank == 0:
-        plotdat(lattice,pflag,nmax)
+
     # Create arrays to store energy, acceptance ratio and order parameter
     energy = np.zeros(nsteps+1)
     ratio = np.zeros(nsteps+1)
@@ -339,24 +225,25 @@ def main(program, nsteps, nmax, temp, pflag):
     if rank == 0:
         final = time.time()
         runtime = final-initial
+        return runtime
 
-        # Final outputs
-        print("{}: Size: {:d}, Steps: {:d}, T*: {:5.3f}: Order: {:5.3f}, Time: {:8.6f} s".format(program, nmax,nsteps,temp,order[nsteps-1],runtime))
-        # Plot final frame of lattice and generate output file
-        savedat(lattice,nsteps,temp,runtime,ratio,energy,order,nmax)
-        plotdat(lattice,pflag,nmax)
-#=======================================================================
-# Main part of program, getting command line arguments and calling
-# main simulation function.
-#
-if __name__ == '__main__':
-    if int(len(sys.argv)) == 5:
-        PROGNAME = sys.argv[0]
-        ITERATIONS = int(sys.argv[1])
-        SIZE = int(sys.argv[2])
-        TEMPERATURE = float(sys.argv[3])
-        PLOTFLAG = int(sys.argv[4])
-        main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG)
-    else:
-        print("Usage: python {} <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>".format(sys.argv[0]))
-#=======================================================================
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+lat_size = np.arange(10, 101, 10)
+iter_size = np.arange(100, 1001, 100)
+
+lat_time = np.zeros(10)
+iter_time = np.zeros(10)
+for i in range(10):
+    lat_time[i] = main(20, lat_size[i], 0.5, comm)
+    iter_time[i] = main(iter_size[i], 20, 0.5, comm)
+
+if rank == 0:
+    lat_dataframe = pd.DataFrame(index = lat_size, columns = [f'MPI MCstep: {size} core'], data = lat_time)
+    iter_dataframe = pd.DataFrame(index = iter_size, columns = [f'MPI MCstep: {size} core'], data = iter_time)
+
+    lat_dataframe.to_csv(f'./times/lat_mpi_MCstep_{size}_core.csv')
+    iter_dataframe.to_csv(f'./times/iter_mpi_MCstep_{size}_core.csv')
